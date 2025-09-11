40aabf2355d03229ae339a0cf45fd2e1857091ef
noshankjain <noshankjain@gmail.com>
2025-09-11 19:12:21 +0530

    Update variable_framing.c

--- variable_framing.c
+++ variable_framing.c
@@ -1,36 +1,25 @@
-#include<string.h>
-#include<stdio.h>
-int main()
-{
-        char str[100];
-        int n,i,j,c=0,count=0;
-        printf("enter the string: ");
-        scanf("%s",str);
-        printf("enter no of frames: ");
-        scanf("%d",&n);
-        int frames[n];
-        printf("enter the size of frames: ");
-        for(i=0;i<n;i++)
-        {
-                printf("Frame %d: ",i);
-                scanf("%d",&frames[i]);
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int n, i, j, k = 0;
+    printf("Enter the string: ");
+    scanf("%s", str);
+    printf("Enter number of frames: ");
+    scanf("%d", &n);
+    int frames[n];
+    printf("Enter the size of each frame:\n");
+    for(i = 0; i < n; i++) {
+        printf("Frame %d size: ", i);
+        scanf("%d", &frames[i]);
+    }
+    printf("\nFrames:\n");
+    for(i = 0; i < n && k < strlen(str); i++) {
+        for(j = 0; j < frames[i] && k < strlen(str); j++) {
+            printf("%c", str[k]);
+            k++;
         }
-        printf("\n the no of frames is: %d\n",n);
-        for(i=0;i<n;i++)
-        {
-                printf("the content of frame %d: ",i);
-                j=0;
-                while(c<strlen(str) && j<frames[i])
-                {
-                        printf("%c",str[c]);
-                        if(str[c]!='\0')
-                        {
-                                count++;
-                        }
-                        c=c+1;
-                        j=j+1;
-                }
-                printf("\n size of frame %d: %d\n\n",i,count);
-                count=0;
-        }
+        printf("\n");
+    }
+    return 0;
 }
